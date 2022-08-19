a26d55e85fda25d4d01e56f69af025b46480ee89
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-19 19:38:05 +0430

    check if it will rain in the next 12 hours. use the python.slice(), and create a for loop for checking the id of every 12 hours.

--- Day 35/Sending SMS via the Twilio API.py
+++ Day 35/Sending SMS via the Twilio API.py
@@ -2,7 +2,7 @@
 import requests
 
 OWM_Endpoint = "http://api.openweathermap.org/data/2.5/onecall"
-api_key = "6d238a4999ad9329c7da25f1196c3933"
+api_key = "b6352b3dca0e8a98600ef53301a56b89"
 
 weather_params = {
     "lat": 29.6103,
@@ -14,3 +14,15 @@ weather_params = {
 response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = (response.json())
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
+# print(weather_data["hourly"][0]["weather"][0]["id"])
